877d558cdd8ac08af1d71b417b44bcc00c19f5f5
Anna Smolinska <anias05@tlen.pl>
2019-05-18 12:00:36 +0100

    ciag arytmetyczny ver2

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,25 @@
 package com.sda.algorytmy;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a1= scanner.nextInt();
+        int d = scanner.nextInt();
+        int ciag = 0;
+        for (int i =2; i<=n; i++){
+            ciag= a1+(i-1)* d;
+            System.out.print(ciag +" ");
+        }
+
     }
 }
